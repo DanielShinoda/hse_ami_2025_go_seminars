1301b212f405f4e89cf35aefa7649efa25b59073
avtereshko <avtereshko@edu.hse.ru>
2025-09-19 12:43:32 +0300

    sem2

--- src/seminar2/tasks/count_vowels.go
+++ src/seminar2/tasks/count_vowels.go
@@ -1,6 +1,15 @@
 package tasks
 
 // CountVowels подсчитывает количество гласных в строке
-func CountVowels(_ string) int {
-	return 0
+func CountVowels(s string) int {
+    count := 0
+    for _, w := range s {
+        for _, w1 := range "aeiouAEIOU" {
+            if w == w1 {
+                count += 1
+                break
+            }
+        }
+    }
+    return count
 }
