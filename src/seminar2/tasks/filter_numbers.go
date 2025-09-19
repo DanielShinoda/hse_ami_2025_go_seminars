6b3b468d7155aaaa5b1aad2f9f71a665750be877
koeebeth <96339397+koeebeth@users.noreply.github.com>
2025-09-19 12:21:00 +0300

    make filter_numbers

--- src/seminar2/tasks/filter_numbers.go
+++ src/seminar2/tasks/filter_numbers.go
@@ -1,6 +1,9 @@
 package tasks
 
+import "slices"
+
 // FilterNumbers фильтрует числа по условию
 func FilterNumbers(numbers []int, predicate func(int) bool) []int {
+	numbers = slices.DeleteFunc(numbers, func(el int) bool { return !predicate(el) })
 	return numbers
 }
