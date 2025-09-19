c20d2ef9c560e032999d5b602cbdfb02c0b3e42f
Stanislav Tomulets <svtomulets@edu.hse.ru>
2025-09-19 12:11:59 +0300

    feat: seminar2 solutions

--- src/seminar2/tasks/filter_numbers.go
+++ src/seminar2/tasks/filter_numbers.go
@@ -2,5 +2,11 @@ package tasks
 
 // FilterNumbers фильтрует числа по условию
 func FilterNumbers(numbers []int, predicate func(int) bool) []int {
-	return numbers
+	result := []int{}
+	for _, it := range numbers {
+		if predicate(it) {
+			result = append(result, it)
+		}
+	}
+	return result
 }
