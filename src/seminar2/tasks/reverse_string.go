2f03f6bde5973c9eec4565270409e6f78eb780d1
Katya <kibanova16@mail.ru>
2025-09-21 00:09:17 +0300

    reverse_string1

--- src/seminar2/tasks/reverse_string.go
+++ src/seminar2/tasks/reverse_string.go
@@ -1,6 +1,11 @@
 package tasks
 
 // ReverseString переворачивает строку
-func ReverseString(_ string) string {
-	return ""
+func ReverseString(s string) string {
+	runes := []rune(s)
+	ans := ""
+	for i := len(runes) - 1; i >= 0; i-- {
+		ans += string(runes[i])
+	}
+	return ans
 }
