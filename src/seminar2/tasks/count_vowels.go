b0703fc52ee360f514ba5ef53f6e0d71950ee2e6
Minin Maksim <min.max.vlad@mail.ru>
2025-09-26 10:31:31 +0300

    Первый сем бам

--- src/seminar2/tasks/count_vowels.go
+++ src/seminar2/tasks/count_vowels.go
@@ -1,6 +1,17 @@
 package tasks
 
+import "unicode"
+
 // CountVowels подсчитывает количество гласных в строке
-func CountVowels(_ string) int {
-	return 0
+
+func CountVowels(s string) int {
+	cnt := 0
+	for _, i := range s {
+		lowreg := unicode.ToLower(i)
+		switch lowreg {
+		case 'a', 'e', 'i', 'o', 'u':
+			cnt++
+		}
+	}
+	return cnt
 }
--- src/seminar2/tasks/invert_map.go
+++ src/seminar2/tasks/invert_map.go
@@ -1,5 +1,9 @@
 package tasks
 
-func invertMap[K comparable, V comparable](_ map[K]V) map[V]K {
-	return make(map[V]K)
+func invertMap[K comparable, V comparable](ourMap map[K]V) map[V]K {
+	new_map := make(map[V]K)
+	for key, value := range ourMap {
+		new_map[value] = key
+	}
+	return new_map
 }
